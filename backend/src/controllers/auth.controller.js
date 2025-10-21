739f068bafbe1049f0808930ce426bea4e23b101
tunenj <yakubyusuf6@gmail.com>
2025-10-21 10:57:04 +0100

    login & logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
 import { generateToken } from "../lib/utils.js"
+import { ENV } from "../lib/env.js"
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body
@@ -41,9 +43,14 @@ export const signup = async (req, res) => {
                 fullName:newUser.fullName,
                 email:newUser.email,
                 profilePic:newUser.profilePic,     
-            })
+            });
 
             // todo: send a welcome email to user
+            try {
+                await sendWelcomeEmail(newUser.email, newUser.fullName, ENV.CLIENT_URL);
+            } catch (error) {
+                console.error("Failed to send welcome email:", error);
+            }
         }else{
             res.status(400).json({message: "Invalid user data"})
         }
@@ -51,4 +58,36 @@ export const signup = async (req, res) => {
         console.log("Error in signup controller:", error)
         res.status(500).json({message:"Internal server error"})
     }
-}
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email });
+        if(!user) 
+            return res.status(400).json({ message:"invalid credentials" })
+            // never tell the client which one is incorrect: password or email
+
+        const isPasswordCorrect = await bcrypt.compare(password,user.password)
+        if(!isPasswordCorrect) return  res.status(400).json({message:"invalid credentials"});
+
+        generateToken(user._id,res)
+
+         res.status(200).json({
+                _id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                profilePic: user.profilePic,     
+            });
+    } catch (error) {
+        console.error("Error in login controller",error)
+        res.status(500).json({message:"Internal server error"})
+    }
+};
+
+
+export const logout = (_, res) => {
+    res.cookie("jwt","",{maxAge:0})
+    res.status(200).json({message:"Logged out successfully"})
+};
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken"
+import { ENV } from "../lib/env.js"
 
 export const generateToken = (userId,res) => {
-    const token = jwt.sign({userId:userId}, process.env.JWT_SECRET, {
+    const token = jwt.sign({userId:userId}, ENV.JWT_SECRET, {
         expiresIn: "7d",
     })
 
@@ -9,7 +10,7 @@ export const generateToken = (userId,res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, 
         httpOnly: true, // prevent XSS attacks: cross-site scripting
         sameSite: "strict", // CSRF atacks
-        secure: process.env.NODE_ENV === "development" ? false : true,
+        secure: ENV.NODE_ENV === "development" ? false : true,
     });
 
     return token;
